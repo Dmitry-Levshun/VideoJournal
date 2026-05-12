94979d67f4167dfc82594e57ab76d30587398726
Dmitry <dlevshun@gmail.com>
2026-05-12 17:44:25 +0300

    Refactor JAVA_HOME handling in gradlew.bat
    Remove hardcoded JAVA_HOME path and clean up comments.

--- gradlew.bat
+++ gradlew.bat
@@ -37,9 +37,7 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-@rem set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_HOME=C:\Android\Android Studio\jbr
-@rem Find java.exe
+set JAVA_HOME=%JAVA_HOME:"=%
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
